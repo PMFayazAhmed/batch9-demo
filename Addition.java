da135b4d01969c4ea13dabeb4b1224452db9a9fa
Fayaz <fana.fayaz@gmail.com>
2022-04-27 19:17:41 +0530

    Added one java file to add two numbers

---
+++ Addition.java
@@ -0,0 +1,13 @@
+class Addition
+{
+public static void main(String args[]){
+	int num1,num2,res;
+	System.out.println("Enter first number");
+	Scanner s=new Scanner(System.in);
+	num1=s.nextInt();
+	System.out.prinrln("Enter second number");
+	num2=s.nextInt();
+	res=num1+num2;
+	System.out.println("Result is "+res);
+}
+}
