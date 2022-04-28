c623d0e595e6740a3b08e7448249cd697da93871
Fayaz <fana.fayaz@gmail.com>
2022-04-28 07:52:41 +0530

    added one sop statement

--- Sum.java
+++ Sum.java
@@ -5,6 +5,7 @@ public static void main(String args[]){
 	System.out.println("Enter first number");
 	Scanner s=new Scanner(System.in);
 	num1=s.nextInt();
+	System.out.printlln("Thankyou");
 	System.out.prinrln("Enter second number");
 	num2=s.nextInt();
 	res=num1+num2;
