68227c1454959d8bcfa0c558e003533d16e6e6f0
Fayaz <fana.fayaz@gmail.com>
2022-04-28 07:50:21 +0530

    Modified sum.java

--- Sum.java
+++ Sum.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 	System.out.println("Enter first number");
 	Scanner s=new Scanner(System.in);
 	num1=s.nextInt();
-	System.out.prinrln("Enter second number");
+	System.out.prinrln("Enter second value");
 	num2=s.nextInt();
 	res=num1+num2;
 	System.out.println("Result is "+res);
